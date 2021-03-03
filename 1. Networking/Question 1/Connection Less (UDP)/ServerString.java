02b7116e519766753f4c0a1855cba7dcf677d018
urvesh254 <urveshpatel2001@gmail.com>
2021-03-04 00:49:58 +0530

    Implement with UDP. Some problem is still there in digits.

---
+++ 1. Networking/Question 1/Connection Less (UDP)/ServerString.java
@@ -0,0 +1,48 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class ServerString {
+
+    public static void main(String args[]) {
+        int SENDER_PORT = 3333;
+        int RECEIVER_PORT = 3334;
+        try {
+            DatagramSocket ds = new DatagramSocket(RECEIVER_PORT);
+
+            byte[] rd = new byte[1024];
+            byte[] sd = new byte[1024];
+
+            DatagramPacket dp = new DatagramPacket(rd, rd.length);
+            // Receive the packet from the sender.
+            ds.receive(dp);
+
+            String string = new String(dp.getData()).trim();
+
+            InetAddress address = InetAddress.getByName("localhost");
+
+            // Find the toatl characters in string and send to the client.
+            int totalCharacters = string.length();
+            sd = Integer.toString(totalCharacters).getBytes();
+            DatagramPacket dp_total_characters = new DatagramPacket(sd, sd.length, address, SENDER_PORT);
+            ds.send(dp_total_characters);
+
+            // Find the toatl digits in string and send to the client.
+            int totalDigits = 0;
+            for (int i = 0; i < string.length(); i++) {
+                char ch = string.charAt(i);
+                if (ch >= '0' && ch <= '9') {
+                    System.out.println(ch);
+                    totalDigits++;
+                }
+            }
+            sd = Integer.toString(totalDigits).getBytes();
+            DatagramPacket dp_total_digits = new DatagramPacket(sd, sd.length, address, SENDER_PORT);
+            ds.send(dp_total_digits);
+
+            ds.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
